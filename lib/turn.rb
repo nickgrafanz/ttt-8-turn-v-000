60039e9dc6f5e9e09c90a8bc77a97ac59e8e2e41
Nick Graffagna <n.graffagna@gmail.com>
2018-06-28 17:08:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ end
 def valid_move?(board,index)
   if board[index] == "" || board[index] == " " && board[index].between?(0,8)
     true
-  else board[index] = "X" || board[index] = "O"
+  else board[index] == "X" || board[index] == "O"
     false
   end
 end
