c3d3b8313a813d6419472bbd5858ae3fe627a3c2
Nick Graffagna <n.graffagna@gmail.com>
2018-06-28 19:48:41 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+require "pry"
+def display_board(board)
+  puts "  #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input = (user_input.to_i)-1
+end
+
+def valid_move?(board,index)
+  if index.between?(0,8) && (board[index] == "" || board[index] == " ")
+    true
+  else
+    false
+  end
+end
+
+def move(board,index,value)
+  board[index]= value
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board,index)
+    move(board,index, value="X")
+    display_board(board)
+  else
+    turn(board)
+  end
+end
