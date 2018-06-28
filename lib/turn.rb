d6d4de7e1680610c5fb2550a8fdc19df7bd5232b
Nick Graffagna <n.graffagna@gmail.com>
2018-06-28 17:49:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,10 +12,9 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board,index)
-  #binding.pry
   if index.between?(0,8) && board[index] == "" || board[index] == " "
     true
-  else #(board[index] == "X" || board[index] == "O")
+  else (board[index] == "X" || board[index] == "O")
     false
   end
 end
