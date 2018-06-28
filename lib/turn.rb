ec96aeea598f7513c2e3e5cf6eb22a236fa6d8a0
Nick Graffagna <n.graffagna@gmail.com>
2018-06-28 15:53:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,11 @@
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
 end
 def valid_move?(board,index)
   if (board[index] = "" || board[index] = " ") && (board[index]>= 0 && board[index] <= 8)
